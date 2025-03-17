74742e023b1dbed57d146b777fe7c1ed99be28ed
fchaguala <felizardo.chaguala@gmail.com>
2025-03-17 22:29:21 +0200

    Auto-commit: 2025-03-17T20:29:21.740Z

--- controllers/userRoleController.js
+++ controllers/userRoleController.js
@@ -30,13 +30,34 @@ export const assignRoleToUser = async (req, res) => {
 
 export const getRolesByUserId = async (req, res) => {
   try {
-    const roles = await UserRole.findAll({
-      where: { userId: req.params.userId },
-      include: [Role],
+    const user = await User.findOne({
+      where: { id: req.params.userId }, // Filtra pelo ID do usuário
+      include: [
+        {
+          model: Role, // Inclui o modelo Role
+          through: { attributes: [] }, // Não inclui atributos da tabela intermediária
+          attributes: ['id', 'name'], // Seleciona apenas os campos necessários
+        },
+      ],
     });
-    res.json(roles);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Retorna os dados do usuário e suas roles
+    const response = {
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      roles: user.roles, // Inclui as roles associadas ao usuário
+    };
+
+    res.json(response);
   } catch (error) {
-    console.error(error); // Log do erro
+    console.error('Erro ao buscar roles:', error); // Log do erro
     res.status(500).json({ error: 'Failed to fetch roles for user' });
   }
 };
@@ -65,3 +86,105 @@ export const getUsersByRoleId = async (req, res) => {
     return res.status(500).json({ error: 'Erro ao buscar usuários.' });
   }
 };
+
+export const getAllUsersWithRoles = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      include: [
+        {
+          model: Role,
+          through: { attributes: [] }, // Não inclui atributos da tabela intermediária
+          attributes: ['id', 'name'], // Seleciona apenas os campos necessários
+        },
+      ],
+    });
+
+    if (!users.length) {
+      return res.status(404).json({ error: 'No users found' });
+    }
+
+    // Mapeia os usuários para incluir suas roles
+    const response = users.map(user => ({
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      roles: user.roles, // Inclui as roles associadas ao usuário
+    }));
+
+    res.json(response);
+  } catch (error) {
+    console.error('Erro ao buscar usuários com roles:', error);
+    res.status(500).json({ error: 'Failed to fetch users with roles' });
+  }
+};
+
+export const getAllDrivers = async (req, res) => {
+  try {
+    const drivers = await User.findAll({
+      include: [
+        {
+          model: Role,
+          through: { attributes: [] },
+          attributes: ['id', 'name'],
+          where: { name: 'Motorista' }, // Filtra apenas usuários com a role 'DRIVER'
+        },
+      ],
+    });
+
+    if (!drivers.length) {
+      return res.status(404).json({ error: 'No drivers found' });
+    }
+
+    // Mapeia os usuários para incluir suas roles
+    const response = drivers.map(user => ({
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      roles: user.roles,
+    }));
+
+    res.json(response);
+  } catch (error) {
+    console.error('Erro ao buscar motoristas:', error);
+    res.status(500).json({ error: 'Failed to fetch drivers' });
+  }
+};
+
+export const getAllClients = async (req, res) => {
+  try {
+    const drivers = await User.findAll({
+      include: [
+        {
+          model: Role,
+          through: { attributes: [] },
+          attributes: ['id', 'name'],
+          where: { name: 'Client' }, // Filtra apenas usuários com a role 'DRIVER'
+        },
+      ],
+    });
+
+    if (!drivers.length) {
+      return res.status(404).json({ error: 'No drivers found' });
+    }
+
+    // Mapeia os usuários para incluir suas roles
+    const response = drivers.map(user => ({
+      id: user.id,
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      roles: user.roles,
+    }));
+
+    res.json(response);
+  } catch (error) {
+    console.error('Erro ao buscar motoristas:', error);
+    res.status(500).json({ error: 'Failed to fetch drivers' });
+  }
+};
+
